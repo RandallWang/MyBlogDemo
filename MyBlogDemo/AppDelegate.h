1a6a4f43d6f675c0a468b3558655449298474e0f
wangtianchi <wangtianchi@nutstore.net>
2018-08-09 14:51:04 +0800

    Initial Commit

---
+++ MyBlogDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBlogDemo
+//
+//  Created by Tianchi Wang on 2018/8/9.
+//  Copyright © 2018 Tianchi Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyBlogDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MyBlogDemo
+//
+//  Created by Tianchi Wang on 2018/8/9.
+//  Copyright © 2018 Tianchi Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
