21a23199eb7107b771bb9af7a843c3de4e194598
wangtianchi <wangtianchi@nutstore.net>
2018-08-11 21:39:04 +0800

    refactor

--- MyBlogDemo/Copy,Strong,DeepCopy,ShallowCopy/TCCopyViewController.m
+++ MyBlogDemo/Copy,Strong,DeepCopy,ShallowCopy/TCCopyViewController.m
@@ -13,7 +13,8 @@
 @property (nonatomic, strong)NSString *strongString;
 @property (nonatomic, copy) NSString *stringCopy;
 
-
+@property (nonatomic, strong)NSArray *arrayStrong;
+@property (nonatomic, copy)NSArray *arrayCopy;
 
 @end
 
@@ -22,34 +23,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
     
+    [self copyAndStrongStringTest];
+}
+
+- (void)copyAndStrongStringTest {
     NSString *testString = @"test String";
-    
-    NSMutableString *testMutableString = [@"test Mutable String" mutableCopy];
-    
     self.strongString = testString;
     self.stringCopy = testString;
     
-    NSLog(@"*********************");
+    NSLog(@"*********NSString Test************");
+    NSLog(@"address:%p",testString);
     NSLog(@"address:%p,value:%@", self.strongString, self.strongString);
     NSLog(@"address:%p,value:%@", self.stringCopy, self.stringCopy);
     NSLog(@"*********************");
     testString = @"changed test string";
+    NSLog(@"address:%p",testString);
     NSLog(@"address:%p,value:%@", self.strongString, self.strongString);
     NSLog(@"address:%p,value:%@", self.stringCopy, self.stringCopy);
     
+    NSMutableString *testMutableString = [@"test Mutable String" mutableCopy];
     self.strongString = testMutableString;
     self.stringCopy = testMutableString;
-
-    NSLog(@"*********************");
+    
+    NSLog(@"*********NSMutableString Test************");
+    NSLog(@"address:%p",testMutableString);
     NSLog(@"address:%p,value:%@", self.strongString, self.strongString);
     NSLog(@"address:%p,value:%@", self.stringCopy, self.stringCopy);
     NSLog(@"*********************");
     [testMutableString appendString:@"changed!!!"];
+    NSLog(@"address:%p",testMutableString);
     NSLog(@"address:%p,value:%@", self.strongString, self.strongString);
     NSLog(@"address:%p,value:%@", self.stringCopy, self.stringCopy);
     NSLog(@"*********************");
+}
 
+- (void)copyAndStrongArrayTest {
+    
 }
 
 @end
