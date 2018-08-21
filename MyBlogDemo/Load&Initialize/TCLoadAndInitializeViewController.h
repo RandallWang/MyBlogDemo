9111eb88ff9d344ac392170581f84ffe624ca974
wangtianchi <wangtianchi@nutstore.net>
2018-08-21 11:21:27 +0800

    add +Load demo

---
+++ MyBlogDemo/Load&Initialize/TCLoadAndInitializeViewController.h
@@ -0,0 +1,17 @@
+//
+//  TCLoadAndInitializeViewController.h
+//  MyBlogDemo
+//
+//  Created by Tianchi Wang on 2018/8/21.
+//  Copyright © 2018 Tianchi Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TCLoadAndInitializeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
